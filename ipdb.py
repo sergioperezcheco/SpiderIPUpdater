4ae54271bfa1677865b15e70ec27f8095872fd49
checo <104541981+sergioperezcheco@users.noreply.github.com>
2024-05-27 15:38:37 +0800

    Update ipdb.py

--- ipdb.py
+++ ipdb.py
@@ -4,7 +4,15 @@ import requests
 # 目标URL，感谢IPDB项目
 url = 'https://ipdb.api.030101.xyz/?type=bestproxy'
 
+# Telegram 机器人 API 配置
+telegram_api_token = 'your_api_token'
+telegram_chat_id = 'your_chat_id'  # 用你的Chat ID，不是机器人的Chat ID
 
+# Cloudflare 信息
+api_key = "your_api_key"
+email = "your_email"
+zone_id = "your_zone_id"
+domain = "your_domain"  # 确保这是完整的域名（如cf.111111.xyz）
 
 # 添加请求头
 headers = {
